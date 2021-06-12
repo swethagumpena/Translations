3a1e32e1020a6d34d131d8e7302106bf146dc695
swetha <swetha_gumpena@mckinsey.com>
2021-06-12 18:23:52 +0530

    feat: add mock data to display

---
+++ src/Utils/mockData.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/prefer-default-export */
+export const products = [
+  {
+    name: 'Milk',
+    price: 20,
+    quantity: '1L',
+  },
+  {
+    name: 'Milk',
+    price: 20,
+    quantity: '1L',
+  },
+  {
+    name: 'Milk',
+    price: 20,
+    quantity: '1L',
+  },
+  {
+    name: 'Milk',
+    price: 20,
+    quantity: '1L',
+  },
+  {
+    name: 'Milk',
+    price: 20,
+    quantity: '1L',
+  },
+];
