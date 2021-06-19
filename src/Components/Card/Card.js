41bb453a0fda1127b31cd54639e45a857f155fe7
swetha <swetha_gumpena@mckinsey.com>
2021-06-19 11:35:24 +0530

    feat: add card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -6,11 +6,19 @@ import styles from './Card.module.css';
 const Card = ({
   name, price, quantity,
 }) => (
-  <div>
-    <div className={styles.wrapper}>
+  <div className={styles.wrapper}>
+    <div>
       <div>{name}</div>
       <div>{price}</div>
       <div>{quantity}</div>
+    </div>
+    <div>
+      <div>Count</div>
+      <div className={styles.quantityBtns}>
+        <button type="button" onClick={() => {}}>-</button>
+        <button type="button" onClick={() => {}}>+</button>
+      </div>
+      <button type="button" onClick={() => {}}>Add</button>
     </div>
   </div>
 );
--- src/Containers/HomePage/HomePage.js
+++ src/Containers/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import React, { useContext } from 'react';
 import { FormattedMessage, FormattedNumber } from 'react-intl';
 import { products } from '../../Utils/mockData';
 import Card from '../../Components/Card/Card';
+import Cart from '../../Components/Cart/Cart';
 import styles from './HomePage.module.css';
 import messages from './messages';
 import { Context } from '../../i18n/provider';
@@ -11,32 +12,37 @@ import { Context } from '../../i18n/provider';
 const HomePage = () => {
   const context = useContext(Context);
   return (
-    <div className={styles.sideMenu}>
-      <div className={styles.cardWrapper}>
-        {products.map((product) => (
-          <Card
-            name={(
-              <FormattedMessage
-                {...messages.name}
-                values={{ nm: product.name }}
-              />
+    <div className={styles.homePageWrapper}>
+      <div className={styles.sideMenu}>
+        <div className={styles.cardWrapper}>
+          {products.map((product) => (
+            <Card
+              name={(
+                <FormattedMessage
+                  {...messages.name}
+                  values={{ nm: product.name }}
+                />
           )}
-            price={(
-              <FormattedNumber
-                value={product.price}
-                style="currency"
-                currencyDisplay="symbol"
-                currency={context.currency}
-              />
+              price={(
+                <FormattedNumber
+                  value={product.price}
+                  style="currency"
+                  currencyDisplay="symbol"
+                  currency={context.currency}
+                />
           )}
-            quantity={(
-              <FormattedMessage
-                {...messages.quantity}
-                values={{ count: product.quantity }}
-              />
+              quantity={(
+                <FormattedMessage
+                  {...messages.quantity}
+                  values={{ count: product.quantity }}
+                />
           )}
-          />
-        ))}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="bodyWrapper">
+        <Cart />
       </div>
     </div>
   );
